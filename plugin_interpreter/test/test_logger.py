9d8d3a3d5d9d492594a34d685c6c619c34f908a5
themanzi <christophermmanzi@protonmail.com>
2018-05-07 13:20:21 -0500

    logger error test

--- plugin_interpreter/test/test_logger.py
+++ plugin_interpreter/test/test_logger.py
@@ -33,6 +33,13 @@ def dummy_proc(logger):
             time()
         ])
 
+def dummy_proc_wrong(logger):
+    sleep(0.5)
+    logger.send([
+        __name__,
+        "Bad log",
+        time()
+    ])
 
 @fixture(scope="module")
 def log():
@@ -82,7 +89,25 @@ def test_to_log_no_output(log, file_handler):
     output = file_handler.read()
     assert not output
 
-def test_logger_start(log):
+def test_log_exception(log):
+    with raises(IndexError):
+        log._to_log([["test", "shortlog"]])
+    with raises(TypeError):
+        log._to_log([[
+            "test",
+            "wrongtime",
+            10,
+            "not a unix epoch float"
+        ]])
+    with raises(TypeError):
+        log._to_log([[
+            "test",
+            "wrongcode",
+            60,
+            "the code doesnt exist"
+        ]])
+
+def test_logger_start_log(log):
     signal = Value(c_bool, False)
     procs, pipes = [], []
     for _ in range(5):
@@ -95,8 +120,21 @@ def test_logger_start(log):
     assert logger_proc.is_alive()
     for proc in procs:
         proc.start()
-    sleep(5)   
+    sleep(5)
     for proc in procs:
         proc.terminate()
     assert logger_proc.is_alive()
     signal.value = True
+
+def test_logger_start(log):
+    signal = Value(c_bool, False)
+    send, recv = Pipe()
+    bad_logger = Process(target=dummy_proc_wrong, args=(send,))
+    log.pipes = [recv]
+    logger_proc = Process(target=log.start, args=(None, signal))
+    logger_proc.start()
+    assert logger_proc.is_alive()
+    bad_logger.start()
+    sleep(2)
+    bad_logger.terminate()
+    assert not logger_proc.is_alive()
