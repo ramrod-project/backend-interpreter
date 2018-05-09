6916e67f05eeabe813e463ddd01d54eaa4091184
Dan Bauman <dan@bauman.space>
2018-05-09 15:45:58 -0500

    full test environment for harness

---
+++ plugin_interpreter/test/test_harness_app.py
@@ -0,0 +1,86 @@
+"""Unit testing for the supervisor module.
+"""
+
+from ctypes import c_bool
+from multiprocessing import connection, Pipe, Value
+from os import environ
+
+from pytest import fixture, raises
+import docker
+
+CLIENT = docker.from_env()
+
+from src import central_logger, controller_plugin, linked_process, rethink_interface, supervisor
+
+
+@fixture(scope="module")
+def sup():
+    environ["LOGLEVEL"] = "DEBUG"
+    CLIENT.containers.run(
+        "ramrodpcp/database-brain",
+        name="rethinkdb",
+        detach=True,
+        ports={"28015/tcp": 28015},
+        remove=True,
+    )
+    sup = supervisor.SupervisorController("Harness")
+    yield sup
+    try:
+        environ["LOGLEVEL"] = ""
+        containers = CLIENT.containers.list()
+        for container in containers:
+            if container.name == "rethinkdb":
+                container.stop()
+                break
+        sup.teardown(0)
+    except SystemExit:
+        pass
+    raise StopIteration
+
+
+def test_supervisor_setup(sup):
+    """Test the Supervisor class.
+    """
+    # DEV environment test
+    assert isinstance(sup, supervisor.SupervisorController)
+    environ["STAGE"] = ""
+    with raises(KeyError):
+        sup.create_servers()
+    environ["STAGE"] = "DEV"
+    assert isinstance(sup.plugin, controller_plugin.ControllerPlugin)
+
+
+def test_supervisor_server_creation(sup):
+    # Test server creation
+    sup.create_servers()
+    sup.db_interface.host = "127.0.0.1"
+
+    for proc in [sup.logger_process, sup.db_process, sup.plugin_process]:
+        assert isinstance(proc, linked_process.LinkedProcess)
+
+    assert isinstance(sup.plugin, controller_plugin.ControllerPlugin)
+    assert isinstance(sup.db_interface, rethink_interface.RethinkInterface)
+    assert isinstance(sup.logger_instance, central_logger.CentralLogger)
+
+
+def test_supervisor_server_spawn(sup):
+    # Test server supawning
+    sup.spawn_servers()
+
+    for proc in [sup.logger_process, sup.db_process, sup.plugin_process]:
+        assert proc.is_alive()
+
+
+if __name__ == "__main__":
+    sup_gen = sup()
+    s = sup_gen.__next__()
+    test_supervisor_setup(s)
+    test_supervisor_server_creation(s)
+    test_supervisor_server_spawn(s)
+
+    try:
+        while True:
+            from time import sleep
+            sleep(20)
+    except KeyboardInterrupt:
+        sup_gen.__next__()
