70ab8613339588e4fc9c534d76d7b549abecc668
themanzi <christophermmanzi@protonmail.com>
2018-05-10 10:12:58 -0500

    added test data

--- plugin_interpreter/test/test_controllerplugin.py
+++ plugin_interpreter/test/test_controllerplugin.py
@@ -3,11 +3,41 @@
 
 from multiprocessing import Queue
 from pytest import fixture, raises
+from time import time
 
 TO_PLUGIN, FROM_PLUGIN = Queue(), Queue()
 
+SAMPLE_TARGET = {
+    "id": "w93hyh-vc83j5i-v82h54u-b6eu4n",
+    "PluginName": "SamplePlugin",
+    "Location": "127.0.0.1",
+    "Port": "8080",
+    "Optional": {
+        "TestVal1": "Test value",
+        "TestVal2": "Test value 2"
+    }
+}
+
+SAMPLE_JOB = {
+    "id": "138thg-eg98198-sf98gy3-feh8h8",
+    "JobTarget": SAMPLE_TARGET,
+    "Status": "Ready",
+    "StartTime": int(time()),
+    "JobCommand": "Do stuff"
+}
+
 def dummy_interface():
-    pass
+    next_item = FROM_PLUGIN.get()
+    if next_item["type"] == "job_request":
+        TO_PLUGIN.put({
+            SAMPLE_JOB
+        })
+    elif next_item["type"] == "functionality":
+        return next_item["data"]
+    elif next_item["type"] == "job_response":
+        return next_item["data"]
+    else:
+        return None
 
 @fixture(scope="module")
 def plugin_base():
