39c510b4f48af5d850ea315a26a73bdcbc930117
lhitch <lhitch08@aol.com>
2018-05-15 13:56:45 -0500

    changed filter for output cursor

--- plugin_interpreter/test/test_rethinkinterface.py
+++ plugin_interpreter/test/test_rethinkinterface.py
@@ -290,22 +290,22 @@ def test_update_output(rethink):
     job_cursor = rethinkdb.db("Brain").table("Jobs").filter(
             rethinkdb.row["JobTarget"]["PluginName"] == "updater"
             ).pluck("id").run(rethink.rethink_connection)
-    job_id = job_cursor.next()
+    job_obj = job_cursor.next()
     updater = {
-        "job" : job_id.get("id"),
+        "job" : job_obj.get("id"),
         "status": "Pending"
     }
     #test updating without any associated output
     rethink._update_job(updater)
     output_data = {
-        "job": job_id,
+        "job": job_obj,
         "output": content
     }
     rethink._send_output(output_data)
     updater["status"] = "Done"
     rethink._update_job(updater)
     output_cursor = rethinkdb.db("Brain").table("Outputs").filter(
-        rethinkdb.row["OutputJob"]["id"]
+        rethinkdb.row["OutputJob"]["id"] == job_obj.get("id")
     ).run(rethink.rethink_connection)
     print(output_cursor)
     output_status = output_cursor.next().get("OutputJob",{}).get("Status")
