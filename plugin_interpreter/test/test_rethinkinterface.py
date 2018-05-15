0227beda8b325f15107fbcd8000fa7008a0107d5
lhitch <lhitch08@aol.com>
2018-05-15 13:34:45 -0500

    removed pluck and getting nested status

--- plugin_interpreter/test/test_rethinkinterface.py
+++ plugin_interpreter/test/test_rethinkinterface.py
@@ -306,6 +306,6 @@ def test_update_output(rethink):
     rethink._update_job(updater)
     output_cursor = rethinkdb.db("Brain").table("Outputs").filter(
         rethinkdb.row["OutputJob"]["id"]
-    ).pluck({"OutputJob" : ["Status"]}).run(rethink.rethink_connection)
-    output_status = output_cursor.next().get("Status")
+    ).run(rethink.rethink_connection)
+    output_status = output_cursor.next().get("OutputJob",{}).get("Status")
     assert output_status == "Done"
