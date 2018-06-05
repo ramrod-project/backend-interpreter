46275576690400e29c6325466439beb9756fb2c8
themanzi <christophermmanzi@protonmail.com>
2018-06-05 11:16:48 -0500

    refactored and added globals

--- plugin_controller/server.py
+++ plugin_controller/server.py
@@ -4,26 +4,43 @@ This is the main server file for the docker
 interpreter controller.
 
 TODO:
-- multiple plugin names
-- get port/protocol requirement from plugin
+- handle multiple plugins/containers
+- read config for plugin from file
+- dynamic port assignment
+- dynamic ip assignment
 """
 import logging
 from os import environ, path as ospath
 from time import asctime, gmtime, sleep, time
+
+import docker
+
 
 logging.basicConfig(
     filename="logfile",
     filemode="a",
     format='%(date)s %(name)-12s %(levelname)-8s %(message)s'
 )
-
 logger = logging.getLogger("controller")
 logger.addHandler(logging.StreamHandler())
 
-import docker
 CLIENT = docker.from_env()
+INTERPRETER_PATH = ospath.join(
+    "/".join(ospath.abspath(__file__).split("/")[:-2]),
+    "plugin_interpreter"
+)
+if environ["STAGE"] == "DEV":
+    NETWORK_NAME = "test"
+else:
+    NETWORK_NAME = "pcp"
+try:
+    TAG = environ["TRAVIS_BRANCH"]
+except KeyError:
+    TAG = "latest"
+PLUGIN = "Harness"
 
-if __name__ == "__main__":
+
+def set_logging():
 
     logger.setLevel(logging.DEBUG)
     if environ["LOGLEVEL"] == "INFO":
@@ -35,42 +52,47 @@ if __name__ == "__main__":
     elif environ["LOGLEVEL"] == "CRITICAL":
         logger.setLevel(logging.CRITICAL)
 
-    interpreter_path = ospath.join(
-        "/".join(ospath.abspath(__file__).split("/")[:-2]),
-        "plugin_interpreter"
+
+def dev_db():
+
+    CLIENT.networks.create(NETWORK_NAME)
+
+    CLIENT.containers.run(
+        "".join(("ramrodpcp/database-brain:", TAG)),
+        name="rethinkdb",
+        detach=True,
+        ports={"28015/tcp": 28015},
+        network=NETWORK_NAME,
+        remove=True
     )
+    sleep(3)
 
-    try:
-        tag = environ["TRAVIS_BRANCH"]
-    except KeyError:
-        tag = "latest"
-    network_name = "pcp"
 
-    if environ["STAGE"] == "DEV":
-        network_name = "test"
-        CLIENT.networks.create(network_name)
-        CLIENT.containers.run(
-            "".join(("ramrodpcp/database-brain:", tag)),
-            name="rethinkdb",
-            detach=True,
-            ports={"28015/tcp": 28015},
-            network=network_name
-        )
-    sleep(5)
+def launch_container(plugin):
         
     CLIENT.containers.run(
-        "".join(("ramrodpcp/interpreter-plugin:", tag)),
-        name="plugin1",
+        "".join(("ramrodpcp/interpreter-plugin:", TAG)),
         environment={
             "STAGE": environ["STAGE"],
             "LOGLEVEL": environ["LOGLEVEL"],
-            "PLUGIN": "Harness"
+            "PLUGIN": plugin
         },
         detach=True,
-        network=network_name,
+        remove=True,
+        network=NETWORK_NAME,
         ports={"5000/tcp": 5000,
                "5005/tcp": 5005}
     )
+
+
+if __name__ == "__main__":
+
+    set_logging()
+
+    if environ["STAGE"] == "DEV":
+        dev_db()
+
+    launch_container(PLUGIN)
     
     containers = CLIENT.containers.list()
     logger.log(
@@ -100,7 +122,7 @@ if __name__ == "__main__":
                 except:
                     logger.log(
                         20,
-                        container.name + " stopped or not running",
+                        "".join((container.name, " stopped or not running")),
                         extra={
                             'date': asctime(gmtime(time()))
                         }
