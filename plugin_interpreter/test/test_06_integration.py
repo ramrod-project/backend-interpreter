1e819e033d6eef93e0e4938618396efe002bdeb2
themanzi <christophermmanzi@protonmail.com>
2018-06-08 14:58:58 -0500

    fixed integration tests

--- plugin_interpreter/test/test_06_integration.py
+++ plugin_interpreter/test/test_06_integration.py
@@ -107,13 +107,6 @@ class IntegrationTest(controller_plugin.ControllerPlugin):
         """placeholder"""
         exit(0)
 
-
-@fixture(scope="module")
-def file_handler():
-    file_handler = open("logfile", "r")
-    yield file_handler
-    file_handler.close()
-
 @fixture(scope="module")
 def rethink():
     try:
@@ -246,7 +239,7 @@ def test_job_status_update(sup, rethink, connection):
     assert job["id"] == SAMPLE_JOB["id"]
     assert job["Status"] == "Pending"
 
-def test_log_to_logger(sup, rethink, file_handler):
+def test_log_to_logger(sup, rethink):
     """Test logging to the logger
 
     This test logs to the logger from the plugin.
@@ -270,31 +263,32 @@ def test_log_to_logger(sup, rethink, file_handler):
     found_plugin_log = False
     found_rethink_log = False
 
-    output = re.split(" +", file_handler.readline())
-    while output:
-        print(output) #confirms there are 6 other logs in the logger before the above.
-        if re.match(r'(Mon|Tue|Wed|Thu|Fri|Sat|Sun)', output[0]):
-            try:
-                assert re.split(" +", asctime(gmtime(NOW))) == output[:5]
-                assert output[5] == "central"
-                assert output[6] == "CRITICAL"
-                assert output[7].split(":")[0] == "plugin"
-                assert " ".join(output[8:]).split("\n")[0] == "Testing out the logger."
-                found_plugin_log = True
-            except AssertionError:
-                pass
-            try:
-                assert output[5] == "central"
-                assert output[6] == "INFO"
-                assert output[7].split(":")[0] == "dbprocess"
-                assert " ".join(output[8:]).split("\n")[0] == "Succesfully opened connection to Rethinkdb"
-                found_rethink_log = True
-            except AssertionError:
-                pass
-        output = None
+    with open("logfile","r+") as file_handler:
         output = re.split(" +", file_handler.readline())
-        if output[0] == "":
-            break
+        while output:
+            print(output) #confirms there are 6 other logs in the logger before the above.
+            if re.match(r'(Mon|Tue|Wed|Thu|Fri|Sat|Sun)', output[0]):
+                try:
+                    assert re.split(" +", asctime(gmtime(NOW))) == output[:5]
+                    assert output[5] == "central"
+                    assert output[6] == "CRITICAL"
+                    assert output[7].split(":")[0] == "plugin"
+                    assert " ".join(output[8:]).split("\n")[0] == "Testing out the logger."
+                    found_plugin_log = True
+                except AssertionError:
+                    pass
+                try:
+                    assert output[5] == "central"
+                    assert output[6] == "INFO"
+                    assert output[7].split(":")[0] == "dbprocess"
+                    assert " ".join(output[8:]).split("\n")[0] == "Succesfully opened connection to Rethinkdb"
+                    found_rethink_log = True
+                except AssertionError:
+                    pass
+            output = None
+            output = re.split(" +", file_handler.readline())
+            if output[0] == "":
+                break
     assert found_plugin_log 
     assert found_rethink_log
 
