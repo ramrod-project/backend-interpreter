2f0989f08d750da87c8ff2d1fd4d691fed35776d
lhitch <lhitch08@aol.com>
2018-06-08 10:20:29 -0500

    added check to logger to avoid logging before initialized

--- plugin_interpreter/src/rethink_interface.py
+++ plugin_interpreter/src/rethink_interface.py
@@ -372,12 +372,13 @@ class RethinkInterface:
             )
 
     def _log(self, log, level):
-        self.logger.send([
-            "dbprocess",
-            log,
-            level,
-            time()
-        ])
+        if self.logger:
+            self.logger.send([
+                "dbprocess",
+                log,
+                level,
+                time()
+            ])
 
     def _log_db_error(self, err):
         if isinstance(err, rethinkdb.ReqlTimeoutError):
