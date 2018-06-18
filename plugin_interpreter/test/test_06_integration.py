32f3f58a5bba7bc6a6aee7767c2d01264256364f
lhitch <lhitch08@aol.com>
2018-06-18 08:45:22 -0500

    delete contents in output table

--- plugin_interpreter/test/test_06_integration.py
+++ plugin_interpreter/test/test_06_integration.py
@@ -321,6 +321,7 @@ def test_database_connection_succeed(rethink):
 
 def test_update_error(sup, rethink, connection):
     rethinkdb.db("Brain").table("Jobs").delete().run(connection)
+    rethinkdb.db("Brain").table("Outputs").delete().run(connection)
     sleep(5)
     rethinkdb.db("Brain").table("Jobs").insert(SAMPLE_JOB).run(connection)
     sleep(3)
