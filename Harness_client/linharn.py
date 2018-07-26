f2ff382f96b89056a5d0ac2e122e1c29d1d4a692
lhitch <lhitch08@aol.com>
2018-07-26 12:58:16 -0500

    Revert "added port option"
    This reverts commit a3ed01d24569f3863f9c2bf456178bf86af3eb95.

--- Harness_client/linharn.py
+++ Harness_client/linharn.py
@@ -1,16 +1,16 @@
 import requests
 from time import sleep
 MAX_REQUEST_TIMEOUT = 120
-HARNESS_STR = "127.0.0.1:"
+HARNESS_STR = "127.0.0.1:5000"
 
 
-def control_loop(client_info, port=5000):
+def control_loop(client_info):
   client = client_info
   looping = True
   # loop through, GET from server and then act on the command
   while looping:
     try:
-      resp = requests.get("http://{}{}/harness/{}".format(HARNESS_STR, str(port), client), timeout=MAX_REQUEST_TIMEOUT)
+      resp = requests.get("http://{}/harness/{}".format(HARNESS_STR, client), timeout=MAX_REQUEST_TIMEOUT)
       cmd, args = resp.text.split(",",1)
       handle_resp(cmd, args, client)
     except ConnectionError:
