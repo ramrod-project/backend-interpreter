fa0952046f71c03c6c09aa98ffb2d43b79330ac2
lhitch <lhitch08@aol.com>
2018-07-27 08:12:51 -0500

    some more code climate changes

--- Harness_client/linharn.py
+++ Harness_client/linharn.py
@@ -11,8 +11,12 @@ def control_loop(client_info):
     # loop through, GET from server and then act on the command
     while looping:
         try:
-            resp = requests.get("http://{}/harness/{}".format(HARNESS_STR, client), timeout=MAX_REQUEST_TIMEOUT)
-            cmd, args = resp.text.split(",",1)
+            resp = requests.get("http://{}/harness/{}".format(
+                HARNESS_STR,
+                client),
+                timeout=MAX_REQUEST_TIMEOUT
+            )
+            cmd, args = resp.text.split(",", 1)
             handle_resp(cmd, args, client)
             retry = 0
         except requests.exceptions.ConnectionError:
@@ -21,6 +25,7 @@ def control_loop(client_info):
             continue
         if retry > 10:
             looping = False
+
 
 def handle_resp(resp, args, client):
     print(resp)
@@ -32,22 +37,7 @@ def handle_resp(resp, args, client):
         sleep(5)
     elif "list_files" in resp:
         requests.post("http://{}/response/{}".format(HARNESS_STR, client), data={"data": "data.txt\nresponse.exe\n"})
-    elif "put_file":
-        requests.get("http://{}/givemethat/{}/{}".format(HARNESS_STR, client, args), timeout=MAX_REQUEST_TIMEOUT)
-    elif "get_file":
-        pass
-    elif "read_registry":
-        pass
-    elif "delete_registry":
-        pass
-    elif "write_registry":
-        pass
-    elif "create_process":
-        pass
-    elif "terminate_process":
-        pass
-    elif "delete_file":
-        pass
+
 
 if __name__ == "__main__":
     client_info = "C_127.0.0.1_1"
