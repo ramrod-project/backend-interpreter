03e7d3d50b46b2090a2f10e2074a9a861e2bf57d
lhitch <lhitch08@aol.com>
2018-07-30 11:46:11 -0500

    added a print

--- test/test_linharn.py
+++ test/test_linharn.py
@@ -134,6 +134,7 @@ def test_linharn(startup_brain, proc, linux_harn, linux_harn2):
     # input_dict = echo_job["Inputs"][0]
     # input_dict
     echo_job["JobCommand"]["Inputs"][0]["Value"] = "Hello World"
+    print(echo_job)
     inserted = brain.queries.insert_jobs([echo_job], True, brain.connect())
     sleep(15)
     # task = linharn.get_task("C_127.0.0.1_1")
